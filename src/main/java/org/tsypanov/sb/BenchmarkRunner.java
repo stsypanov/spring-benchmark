5827ce128fc781b53d2e06a2c09069fd0fd4ad32
SergiiTsypanov <stsypanov@corelogic.com>
2019-12-06 12:03:09 +0200

    Polish

--- src/main/java/org/tsypanov/sb/BenchmarkRunner.java
+++ src/main/java/org/tsypanov/sb/BenchmarkRunner.java
@@ -5,6 +5,7 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.openjdk.jmh.runner.options.TimeValue;
+import org.tsypanov.sb.aspect.benchmark.AspectPrototypeBenchmark;
 import org.tsypanov.sb.prototype.benchmark.ContextLoadBenchmark;
 import org.tsypanov.sb.prototype.benchmark.PrototypeBenchmark;
 
@@ -12,11 +13,12 @@ public class BenchmarkRunner {
 
   public static void main(String[] args) throws Exception {
     Options opt = new OptionsBuilder()
-            .include(ContextLoadBenchmark.class.getSimpleName())
+            .include(AspectPrototypeBenchmark.class.getSimpleName())
+//            .include(ContextLoadBenchmark.class.getSimpleName())
             .warmupIterations(10)
             .warmupTime(TimeValue.seconds(1))
             .measurementIterations(1)
-            .measurementTime(TimeValue.days(1))
+            .measurementTime(TimeValue.seconds(1))
             .forks(10) //0 makes debugging possible
             .shouldFailOnError(true)
 //				.shouldDoGC(false)
--- src/main/java/org/tsypanov/sb/aspect/config/AspectConfig.java
+++ src/main/java/org/tsypanov/sb/aspect/config/AspectConfig.java
@@ -2,6 +2,7 @@ package org.tsypanov.sb.aspect.config;
 
 
 import org.springframework.aop.aspectj.annotation.AnnotationAwareAspectJAutoProxyCreator;
+import org.springframework.aop.framework.autoproxy.AbstractAutoProxyCreator;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,12 +26,12 @@ public class AspectConfig {
   }
 
   @Bean
-  AnnotationAwareAspectJAutoProxyCreator proxyCreator() {
-    AnnotationAwareAspectJAutoProxyCreator proxyFactoryBean = new AnnotationAwareAspectJAutoProxyCreator();
-    proxyFactoryBean.setProxyTargetClass(true);
+  AbstractAutoProxyCreator proxyCreator() {
+    AbstractAutoProxyCreator factory = new AnnotationAwareAspectJAutoProxyCreator();
+    factory.setProxyTargetClass(true);
     // this is to check the influence of commit commit e44d3dab
     // use Method in fixedInterceptorMap instead of String returned from Method::toString
-    proxyFactoryBean.setFrozen(true);
-    return proxyFactoryBean;
+    factory.setFrozen(true);
+    return factory;
   }
 }
